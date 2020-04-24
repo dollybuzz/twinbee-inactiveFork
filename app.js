d105204fe757bc54aebc53cddd730b2fd5de89cf
DuskEcho <grebrown@csumb.edu>
2020-04-23 17:18:22 -0700

    initial setup

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+const controller = require('./controllers/controller.js');
+const app = express();
+const bodyParser = require('body-parser');
+
+
+
+app.set('view engine', 'ejs');
+app.set('port',  process.env.PORT || "8080");
+app.set('ip',  process.env.IP || "0.0.0.0");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {res.render("home")});
+
+app.listen(app.get('port'), app.get('ip'),()=>{console.log(`Express Server is Running at ${app.get('ip')} on port ${app.get('port')}`);});
