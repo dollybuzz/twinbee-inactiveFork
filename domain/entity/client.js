0254252fcea2c92ebdf970d7bc9ccdba7745a3b1
DuskEcho <grebrown@csumb.edu>
2020-04-29 14:06:33 -0700

    added initial client class

---
+++ domain/entity/client.js
@@ -0,0 +1,11 @@
+export class Client{
+    constructor(id, name, location, remainingHours, email, chargebeeObj, makers){
+        const this.id = this.id;
+        const this.name = this.name;
+        const this.location = this.location;
+        const this.chargebeeObj = chargebeeObj;
+        let this.remainingHours = remainingHours;
+        let this.email = email;
+        let this.makers = makers;
+    }
+}
