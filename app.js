752d4c7a6695896579ee227fa2922fd1ab21a035
DuskEcho <grebrown@csumb.edu>
2020-04-29 12:14:59 -0700

    initial scaffolding for views, viewControllers, and css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ Authors: Dalia Faria, Greg Brown
 
  */
 const express = require("express");
-const controller = require('./controllers/controller.js');
+const landingPageController = require('./controllers/landingPageController.js');
 const app = express();
 const bodyParser = require('body-parser');
 const chargebee = require('chargebee');
@@ -20,7 +20,6 @@ app.set('ip',  process.env.IP || "0.0.0.0");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-app.get("/", (req, res) => {res.render("home")});
+app.get("/", landingPageController.renderLanding);
 
 app.listen(app.get('port'), app.get('ip'),()=>{console.log(`Express Server is Running at ${app.get('ip')} on port ${app.get('port')}`);});
---
+++ controllers/landingPageController.js
@@ -0,0 +1,17 @@
+
+module.exports ={
+    renderLanding: (req, res)=>{
+        let headerImageActual = {
+            src: "/img/freedom-makers-logo.png",
+            alt: "Freedom Makers Logo"
+        };
+        let footerLinks = [
+            {link: "", text: "Our Mission"},
+            {link: "", text: "Become a Maker"},
+        {link: "", text: "FAQ"},
+            {link: "", text: "Problems? Contact us."},
+        ]
+
+        res.render("landing", {headerImg:headerImageActual, navItemsBottom: footerLinks});
+    },
+}
