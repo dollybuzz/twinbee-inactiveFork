dcf2ae18492775593f6b8543b6ab7c491ce33357
DuskEcho <grebrown@csumb.edu>
2020-05-04 14:19:58 -0700

    added add subscription

--- controllers/chargebeeRestController.js
+++ controllers/chargebeeRestController.js
@@ -8,24 +8,25 @@ module.exports = {
         res.end();
     },
     retrievePlan: async function(req, res){
-        let planActual = await chargebeeService.retrievePlan(req.query.planName)
+        let planActual = await chargebeeService.retrievePlan(req.query.planId)
             .catch(err=>console.log(err));
         res.send({plan: planActual});
     },
     updatePlan: function(req, res){
-        chargebeeService.updatePlan(req.body.planName, req.body.newName,
+        chargebeeService.updatePlan(req.body.planId, req.body.newName,
             req.body.planInvoiceName, req.body.planPrice);
         res.end();
     },
     deletePlan: function(req, res){
-        chargebeeService.deletePlan(req.body.planName);
+        chargebeeService.deletePlan(req.body.planId);
         res.end();
     },
 
-
     createSubscription: function(req, res){
-
+        chargebeeService.createSubscription(req.body.planId, req.body.customerId, req.body.planQuantity);
+        res.end();
     },
+
     retrieveSubscription: function(req, res){
 
     },
@@ -34,6 +35,5 @@ module.exports = {
     },
     deleteSubscription: function(req, res){
 
-    },
-
+    }
 }
--- services/chargebeeService.js
+++ services/chargebeeService.js
@@ -5,43 +5,42 @@ chargebee.configure({site : "freedom-makers-test",
 
 //TODO: Add validation
 class ChargebeeService {
-    constructor(){};
+    constructor() {
+    };
 
-    createPlan(planName, invoiceName, pricePerHour, planDescription){
+    createPlan(planName, invoiceName, pricePerHour, planDescription) {
 
-        let planId = planName.replace(/\s+|\.|\,|'|"|&|\$|%|#|@|!/g,"-");
+        let planId = planName.replace(/\s+|\.|\,|'|"|&|\$|%|#|@|!/g, "-");
         chargebee.plan.create({
-            id : planId,
-            name : planName,
-            invoice_name : invoiceName,
+            id: planId,
+            name: planName,
+            invoice_name: invoiceName,
             description: planDescription,
-            price : pricePerHour,
+            price: pricePerHour,
             pricing_model: "per_unit",
             currency_code: "USD",
             period: 1,
             period_unit: "month",
 
-        }).request(function(error,result) {
-            if(error){
+        }).request(function (error, result) {
+            if (error) {
                 //handle error
                 console.log(error);
-            }else{
+            } else {
                 console.log(result);
                 var plan = result.plan;
             }
         });
     }
-    retrievePlan(planName){
 
-        return new Promise((resolve, reject)=>{
-            let planId = planName.replace(/\s+|\.|\,|'|"|&|\$|%|#|@|!/g,"-");
-
-            chargebee.plan.retrieve(planId).request(function(error,result) {
-                if(error){
+    retrievePlan(planId) {
+        return new Promise((resolve, reject) => {
+            chargebee.plan.retrieve(planId).request(function (error, result) {
+                if (error) {
                     //handle error, email us?
                     //console.log(error);
                     reject(error);
-                }else{
+                } else {
                     //console.log(result);
                     var plan = result.plan;
                     resolve(plan);
@@ -49,10 +48,10 @@ class ChargebeeService {
             });
         })
     }
-    updatePlan(planName, newName, planInvoiceName, planPrice) {
+
+    updatePlan(planId, newName, planInvoiceName, planPrice) {
 
         return new Promise((resolve, reject) => {
-            let planId = planName.replace(/\s+|\.|\,|'|"|&|\$|%|#|@|!/g, "-");
 
             chargebee.plan.update(planId, {
                 name: newName,
@@ -71,36 +70,52 @@ class ChargebeeService {
             });
         })
     }
-    deletePlan(planName){
-        let planId = planName.replace(/\s+|\.|\,|'|"|&|\$|%|#|@|!/g, "-");
 
-        chargebee.plan.delete(planId).request(function(error,result) {
-            if(error){
+    deletePlan(planId) {
+
+        chargebee.plan.delete(planId).request(function (error, result) {
+            if (error) {
                 //handle error
                 console.log(error);
-            }else{
+            } else {
                 //console.log(result);
                 var plan = result.plan;
             }
         });
     }
 
-
-    createSubscription(){
+    createSubscription(planId, customerId, planQuantity) {
+        chargebee.subscription.create_for_customer(customerId, {
+            plan_id: planId,
+            plan_quantity: planQuantity,
+            auto_collection: "off"
+        }).request(function (error, result) {
+            if (error) {
+                //handle error
+                console.log(error);
+            } else {
+                console.log(result);
+                var subscription = result.subscription;
+                var customer = result.customer;
+                var card = result.card;
+                var invoice = result.invoice;
+                var unbilled_charges = result.unbilled_charges;
+            }
+        });
 
     }
-    retrieveSubscription(){
 
-    }
-    updateSubscription(){
-
-    }
-    deleteSubscription(){
+    retrieveSubscription() {
 
     }
 
+    updateSubscription() {
 
+    }
 
+    deleteSubscription() {
+
+    }
 
 
 }
