050162fca63f56a7db54306f71eb72064956c111
DuskEcho <grebrown@csumb.edu>
2020-05-07 11:56:41 -0700

    added clock out to timeClockService

--- services/timeClockService.js
+++ services/timeClockService.js
@@ -25,9 +25,41 @@ class TimeClockService {
             }
         })
     }
-
+    
+    /**
+     * "Clocks out" a maker by id
+     * NOTE: This assumes only one sheet should be 'online' at a time.
+     * If multiple are online, all are clocked out.
+     *
+     * @param makerId   - id of maker to clock out
+     */
     async clockOut(makerId){
+        let result = await request(`http://${process.env.IP}:${process.env.PORT}/api/getTimeSheetsByMakerId?id=${makerId}`);
+        let sheetsForMaker = JSON.parse(result.body);
+        let onlineSheets = [];
 
+        // get online sheets
+        for (var i = 0; i < sheetsForMaker.length; ++i){
+            let currentSheet = sheetsForMaker[i];
+            if (currentSheet.timeIn[0] != "0" && currentSheet.timeOut[0] == "0"){
+                onlineSheets.push(currentSheet);
+            }
+        }
+
+        //"clock out" online sheets
+        for (var i = 0; i < onlineSheets.length; ++i){
+            let currentSheet = onlineSheets[i];
+            request({
+                method: 'POST',
+                uri: `http://${process.env.IP}:${process.env.PORT}/api/updateTimeSheet`,
+                form: {
+                    id: currentSheet.id,
+                    hourlyRate: currentSheet.hourlyRate,
+                    timeIn: currentSheet.timeIn,
+                    timeOut: await this.getCurrentMoment()
+                }
+            })
+        }
     }
 
     async getRunningTime(makerId){
