0388301f6307355dff52630beca92f6de498f3cc
DuskEcho <grebrown@csumb.edu>
2020-05-09 19:18:42 -0700

    )

--- public/js/google.js
+++ public/js/google.js
@@ -22,7 +22,7 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     GOOGLE_USER = googleUser;
     var id_token = GOOGLE_USER.getAuthResponse().id_token;
-    window.location.replace(`/login?token=${id_token}`;
+    window.location.replace(`/login?token=${id_token}`);
 }
 
 
