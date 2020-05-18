be28f6da48c77e768530400893478d66b3df4d41
DuskEcho <grebrown@csumb.edu>
2020-05-17 20:38:10 -0700

    google testing

--- public/js/makerPage.js
+++ public/js/makerPage.js
@@ -2,7 +2,6 @@
 let selectedRow = null;
 let selectedTab = null;
 let id_token = null;
-let signOut = null;
 
 let navMapper = {
     main: function () {
@@ -20,7 +19,7 @@ let navMapper = {
 };//end navMapper
 
 function showFunction (functionality, endpoint) {
-    /*
+/*
     $.ajax({
         url: endpoint,
         method: "post",
@@ -138,9 +137,54 @@ function clientFunctionality (res){
     });
 }
 
-gapi.load('auth2', function() {
-    gapi.auth2.init();
-});
+function timeSheetFunctionality (res) {
+    //Create table
+    $("#userMainContent").html(
+        "<div id=\"buttonsTop\"></div>\n" +
+        "<div class='row' id='topRow'>\n" +
+        "<div id=\"floor\">\n" +
+        "    <table id=\"sheetsTable\" class=\"table\">\n" +
+        "    </table>\n" +
+        "</div></div>");
+    $("#sheetsTable").append('\n' +
+        '        <thead class="thead">\n' +
+        '            <th scope="col">#</th>\n' +
+        '            <th scope="col">Maker ID</th>\n' +
+        '            <th scope="col">Client ID</th>\n' +
+        '            <th scope="col">Hourly Rate</th>\n' +
+        '            <th scope="col">Clock In</th>\n' +
+        '            <th scope="col">Clock Out</th>\n' +
+        '            <th scope="col">Occupation</th>\n' +
+        '        </thead><tbody>');
+    //Populate table
+    res.forEach(item => {
+        $("#sheetsTable").append('\n' +
+            '<tr class="sheetRow">' +
+            '   <td scope="row">' + item.id +'</td>' +
+            '   <td>' + item.makerId + '</td>'+
+            '   <td>' + item.clientId + '</td>'+
+            '   <td>' + item.hourlyRate + '</td>'+
+            '   <td>' + item.timeIn + '</td>'+
+            '   <td>' + item.timeOut + '</td>'+
+            '   <td>' + item.occupation + '</td></tr>'
+        );
+    });
+    $("#sheetsTable").append('\n</tbody>');
+
+    //Body Block content
+    createBody();
+
+
+    //Event Listeners
+
+    //Row effect
+    $(".sheetRow").mouseenter(function () {
+        $(this).css('transition', 'background-color 0.5s ease');
+        $(this).css('background-color', '#e8ecef');
+    }).mouseleave(function () {
+        $(this).css('background-color', 'white');
+    });
+}
 
 
 $(document).ready(function () {
@@ -168,20 +212,6 @@ $(document).ready(function () {
         });
     }, 1000);
 
-    $.ajax({
-        url: "/api/getMakerIdByToken",
-        method: "post",
-        data: {
-            token: id_token,
-            auth: id_token
-        },
-        success: function (res, status) {
-            makerId = res.id;
-        },
-        error: function (res, status) {
-            console.log(res)
-        }
-    });
 
     //Event Listeners for other nav menu items
     $(".navItem").click(function (e) {
