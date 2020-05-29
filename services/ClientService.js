c5cf8eeb906bc96aa0eeebae087add917d709c04
DuskEcho <grebrown@csumb.edu>
2020-05-29 10:56:54 -0700

    webhook testing

--- services/ClientService.js
+++ services/ClientService.js
@@ -423,18 +423,23 @@ class ClientService {
         }
     }
 
+    async test(){
+        return "test"
+    }
+
+
 
     async subscriptionRenewed(parsedBody){
         if (parsedBody.event_type === "subscription_renewed") {
             console.log("Subscription renewal request received");
-            return await this.webHookBucketUpdate(parsedBody);
+            return await this.test(parsedBody);
         }
     }
 
     async subscriptionCreated(parsedBody){
         if (parsedBody.event_type === "subscription_created") {
             console.log("Subscription creation request received");
-            return await this.webHookBucketUpdate(parsedBody);
+            return await this.test(parsedBody);
         }
     }
 
