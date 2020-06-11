2a181bda869458c5ab6d591836bfa9617ace5d0c
DuskEcho <grebrown@csumb.edu>
2020-06-11 12:45:16 -0700

    403 fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -139,7 +139,7 @@ module.exports = {
             clientPageController.renderLanding(req, res);
         }
         else{
-            res.send(403);
+            res.render("forbidden");
         }
     }
 };
