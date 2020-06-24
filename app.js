438dfda5cf5f0cb2609f2375bc01fef5ba5a2605
DuskEcho <grebrown@csumb.edu>
2020-06-23 18:43:20 -0700

    testing

--- app.js
+++ app.js
@@ -363,7 +363,7 @@ app.get("/api/getEnvironment",
     (req, res)=>{res.send(process.env.TWINBEE_ENVIRONMENT_FLAG === 'test')});
 
 (async function() {
-    //console.log((await chargebeeservice.getAllSubscriptions()).length);
+    console.log(process.env.CHARGEBEE_API_KEY);
 })();
 
 app.listen(app.get('port'), app.get('ip'),()=>{console.log(`Express Server is Running at ${app.get('ip')} on port ${app.get('port')}`);});
