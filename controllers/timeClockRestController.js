ec8cb1632251afabcd7aba7c21f4e33ffa251a95
DuskEcho <grebrown@csumb.edu>
2020-06-30 17:35:47 -0700

    backend support for clockout task

--- controllers/timeClockRestController.js
+++ controllers/timeClockRestController.js
@@ -21,11 +21,12 @@ module.exports = {
 
     /**
      * ENDPOINT: /api/clockOut
-     * "Clocks out" a given user. Commpletes any timesheets without valid "clock in"
+     * "Clocks out" a given user. Completes any timesheets without valid "clock in"
      * values (ideally should only ever be one) with the current time. Looks for
      * values in the body in the form:
      * {
      *     "makerId": id of the maker to clock out,
+     *     "newTask": updated task (if desired)
      *     "auth": authentication credentials; either master or token
      * }
      *
@@ -36,7 +37,6 @@ module.exports = {
     clockOut: async (req, res) => {
         console.log('Attempting to clock out user from REST:');
         console.log(req.body);
-        await timeClockService.clockOut(req.body.auth);
-        res.send(await timeClockService.clockOut(req.body.auth));
+        res.send(await timeClockService.clockOut(req.body.auth, req.body.newTask));
     }
 }
