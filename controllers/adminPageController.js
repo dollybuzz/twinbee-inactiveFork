ef2ef00a794d6bf88fdf0ce75a49ea858668192b
DuskEcho <grebrown@csumb.edu>
2020-07-01 15:19:11 -0700

    trying to keep scalability

--- controllers/adminPageController.js
+++ controllers/adminPageController.js
@@ -1,5 +1,5 @@
-module.exports ={
-     renderLanding: (req, res)=>{
+module.exports = {
+    renderLanding: (req, res) => {
         let thisID = {
             text: "adminTitle"
         };
@@ -11,21 +11,45 @@ module.exports ={
             alt: "Freedom Makers Logo"
         };
         let headerLinks = [
-            {link: "/admin", text: "Main", id:"main"},
-            {link: "", text: "Manage Clients", id:"manageClients"},
-            {link: "", text: "Manage Freedom Makers", id: "manageMakers"},
-            {link: "", text: "Manage Plans", id: "managePlans"},
-            {link: "", text: "Manage Subscriptions", id: "manageSubscriptions"},
-            {link: "", text: "Manage Relationships", id:"manageRelationships"},
-            {link: "", text: "Manage Available Credit", id: "manageCredit"},
-            {link: "", text: "Reporting Tools", id:"reportingTools"},
-            {link: "", text: "Review TimeSheets", id:"reviewTimeSheets"},
-            {link: "", text: "Run Reports", id:"runReports"},
+            {
+                link: "/admin", text: "Main", id: "main", dropdowns: []
+            },
+            {
+                link: "", text: "Manage Clients", id: "manageClients", dropdowns: []
+            },
+            {
+                link: "", text: "Manage Freedom Makers", id: "manageMakers", dropdowns: []
+            },
+            {
+                link: "", text: "Manage Plans", id: "managePlans", dropdowns: []
+            },
+            {
+                link: "", text: "Manage Subscriptions", id: "manageSubscriptions", dropdowns: []
+            },
+            {
+                link: "", text: "Manage Relationships", id: "manageRelationships", dropdowns: []
+            },
+            {
+                link: "", text: "Manage Available Credit", id: "manageCredit", dropdowns: []
+            },
+            {
+                link: "", text: "Reporting Tools", id: "reportingTools", dropdowns: [
+                    {link: "", text: "Review TimeSheets", id: "reviewTimeSheets"},
+                    {link: "", text: "Run Reports", id: "runReports"}
+                ]
+            }
         ];
         let footerLinks = [
             {link: "", text: "Report a problem", id: "reportProblem"}
         ];
 
-        res.render("admin", {siteTitle:thisID, pageTitle:headerTitle, headerImg:headerImageActual, navItemsTop: headerLinks, navItemsBottom: footerLinks});
+        res.render("admin", {
+            siteTitle: thisID,
+            pageTitle: headerTitle,
+            headerImg: headerImageActual,
+            navItemsTop: headerLinks,
+            navItemsBottom: footerLinks
+        });
     }
-}
+};
+
