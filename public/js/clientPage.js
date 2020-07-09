c8a38d8a8b9def7255db52e6d2099887211cf941
Dalia Faria <43527456+dollybuzz@users.noreply.github.com>
2020-07-08 18:02:24 -1000

    manipulating priority for alerts

--- public/js/clientPage.js
+++ public/js/clientPage.js
@@ -101,6 +101,35 @@ function showFunction(functionality, endpoint) {
 
 //Main Methods
 function showAlerts() {
+
+    $.ajax({
+        url: "/api/getAllMyTimeBuckets",
+        method: "post",
+        data: {
+            auth: id_token,
+            token: id_token,
+        },
+        dataType: "json",
+        success: function (bucketres, bucketstatus) {
+            for(var plan in bucketres.buckets)
+            {
+                if(Number.parseInt(bucketres.buckets[plan]) <= 300 && Number.parseInt(bucketres.buckets[plan]) > 0)
+                {
+                    $("#clientTopRow").html("<div class='alert alert-warning alert-dismissable fade show' role='alert'>You are running low on available hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+                        "<span aria-hidden='true'>&times;</span></button></div>");
+                }
+                else
+                {
+                    $("#clientTopRow").html("<div class='alert alert-danger alert-dismissable fade show' role='alert'>You are out of available hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+                        "<span aria-hidden='true'>&times;</span></button></div>");
+                }
+            }
+        },
+        error: function (invoiceres, invoicestatus) {
+            $("#userMainContent").html("Alerts are not working!");
+        }
+    });
+
     $.ajax({
         url: "/api/doIHaveInvoices",
         method: "post",
@@ -120,37 +149,6 @@ function showAlerts() {
             $("#userMainContent").html("Alerts are not working!");
         }
     });
-
-    $.ajax({
-        url: "/api/getAllMyTimeBuckets",
-        method: "post",
-        data: {
-            auth: id_token,
-            token: id_token,
-        },
-        dataType: "json",
-        success: function (bucketres, bucketstatus) {
-            for(var plan in bucketres.buckets)
-            {
-                if(Number.parseInt(bucketres.buckets[plan]) <= 300 && Number.parseInt(bucketres.buckets[plan]) > 0)
-                {
-                    $("#clientTopRow").html("<div class='alert alert-warning alert-dismissable fade show' role='alert'>You are running low on available hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
-                        "<span aria-hidden='true'>&times;</span></button></div>");
-                }
-                else if(Number.parseInt(bucketres.buckets[plan]) <= 0)
-                {
-                    $("#clientTopRow").append("<div class='alert alert-danger alert-dismissable fade show' role='alert'>You are out of available hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
-                        "<span aria-hidden='true'>&times;</span></button></div>");
-                }
-            }
-        },
-        error: function (invoiceres, invoicestatus) {
-            $("#userMainContent").html("Alerts are not working!");
-        }
-    });
-
-
-
 }
 
 //Available Hours Methods
