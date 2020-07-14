e3bbc62754f89ee495741072ebbb20c615d52acc
DuskEcho <grebrown@csumb.edu>
2020-07-14 11:54:56 -0700

    unit test modification for timesheet/relationship integration

--- test/timeSheetService.spec.js
+++ test/timeSheetService.spec.js
@@ -9,15 +9,15 @@ const nock = require('nock');
 const TimeSheet = require('../domain/entity/timeSheet');
 
 const timeSheetBasic1 = {id: 1, maker_id: 1, client_id: 'a', hourly_rate: 20.00, start_time: '2019-04-24 22:22:22',
-                        end_time: '0000-00-00 00:00:00', task: 'worker', admin_note: 'No details given.'};
+                        end_time: '0000-00-00 00:00:00', task: 'worker', admin_note: 'No details given.', relationship_id: 1};
 const timeSheetBasic2 = {id: 2, maker_id: 1, client_id: 'b', hourly_rate: 20.00, start_time: '2019-04-23 22:22:22',
-                        end_time: '2019-04-23 23:23:23', task: 'worker', admin_note: 'Added by admin: 1'};
+                        end_time: '2019-04-23 23:23:23', task: 'worker', admin_note: 'Added by admin: 1', relationship_id: 2};
 const timeSheetBasic3 = {id: 3, maker_id: 2, client_id: 'a', hourly_rate: 20.00, start_time: '2019-04-22 22:22:22',
-                        end_time: '2019-04-22 23:23:23', task: 'worker', admin_note: 'Added by admin: 2'};
+                        end_time: '2019-04-22 23:23:23', task: 'worker', admin_note: 'Added by admin: 2', relationship_id: 3};
 
-const timeSheetRefined1 = new TimeSheet(1, 1, 20.00, 'a', '2019-04-24 22:22:22', '0000-00-00 00:00:00', 'worker', 'No details given.');
-const timeSheetRefined2 = new TimeSheet(2, 1, 20.00, 'b', '2019-04-23 22:22:22', '2019-04-23 23:23:23', 'worker', 'Added by admin: 1');
-const timeSheetRefined3 = new TimeSheet(3, 2, 20.00, 'a', '2019-04-22 22:22:22', '2019-04-22 23:23:23', 'worker', 'Added by admin: 2');
+const timeSheetRefined1 = new TimeSheet(1, 1, 20.00, 'a', '2019-04-24 22:22:22', '0000-00-00 00:00:00', 'worker', 'No details given.', 1);
+const timeSheetRefined2 = new TimeSheet(2, 1, 20.00, 'b', '2019-04-23 22:22:22', '2019-04-23 23:23:23', 'worker', 'Added by admin: 1', 2);
+const timeSheetRefined3 = new TimeSheet(3, 2, 20.00, 'a', '2019-04-22 22:22:22', '2019-04-22 23:23:23', 'worker', 'Added by admin: 2', 3);
 
 
 
@@ -128,7 +128,7 @@ describe('Time Sheet Service Test', function () {
     });
 
     it('Should create a new valid timesheet', async function () {
-        let actual = await timeSheetService.createTimeSheet(1, 20.00, 'a', '2019-04-24 22:22:22', '0000-00-00 00:00:00', 'worker', "", "");
+        let actual = await timeSheetService.createTimeSheet(1, 20.00, 'a', '2019-04-24 22:22:22', '0000-00-00 00:00:00', 'worker', "No details given.", 1);
         expect(actual).to.deep.equal(timeSheetRefined1);
         sinon.assert.calledOnce(timeSheetRepo.createSheet);
     });
