699d563585fdda47136ce943588c56d96f1e1b2d
DuskEcho <grebrown@csumb.edu>
2020-07-20 15:56:44 -0700

    style

--- services/authService.js
+++ services/authService.js
@@ -5,6 +5,8 @@ const clientId = process.env.GOOGLE_CLIENT_ID;
 const client = new OAuth2Client(clientId);
 const compare = util.promisify(require('bcrypt').compare);
 const emailService = require('./notificationService.js');
+
+
 class AuthService {
     constructor() {
     };
