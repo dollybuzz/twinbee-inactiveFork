afd783afebeb2f27bcb490a83fa40481b13ce28f
Dalia Faria <43527456+dollybuzz@users.noreply.github.com>
2020-07-24 15:11:07 -1000

    adjusting to plan_quantity * plan_unit_price to reflect new charge

--- public/js/clientPage.js
+++ public/js/clientPage.js
@@ -477,7 +477,7 @@ function subscriptionFunctionality(res) {
                 "   <td>" + changes + "</td>" +
                 '   <td>' + (subscription.cancelled_at == undefined ? "No" : moment.unix(subscription.cancelled_at).format('YYYY/MM/DD')) + '</td>' +
                 '   <td>' + (subscription.next_billing_at == undefined ? "Terminated" : moment.unix(subscription.next_billing_at).format('YYYY/MM/DD')) + '</td>' +
-                '   <td>$' + (Math.floor(parseFloat(subscription.plan_amount))/100).toFixed(2) + '</td>' +
+                '   <td>$' + (Math.floor(parseFloat(subscription.plan_quantity*subscription.plan_unit_price))/100).toFixed(2) + '</td>' +
                 '   <td><button type="button" class="btn btn-select btn-circle btn-xl" id="ChangeSubButton">Change</button></td></tr>');
         }
     });
