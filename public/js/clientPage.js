25057631f48bc00c7185a0d48eb8ada111eaef65
Dalia Faria <43527456+dollybuzz@users.noreply.github.com>
2020-07-29 23:22:02 -1000

    break if have no hours

--- public/js/clientPage.js
+++ public/js/clientPage.js
@@ -156,6 +156,7 @@ function showAlerts() {
                 if (Number.parseInt(bucketres.buckets[plan]) <= 0) {
                     $("#clientAlerts").html("<div class='alert alert-danger alert-dismissable fade show' role='alert'>You have no hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
                         "<span aria-hidden='true'>&times;</span></button></div>");
+                    break;
                 } else {
                     if (Number.parseInt(bucketres.buckets[plan]) <= 300 && Number.parseInt(bucketres.buckets[plan]) > 0) {
                         $("#clientAlerts").html("<div class='alert alert-warning alert-dismissable fade show' role='alert'>You are running low on available hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
