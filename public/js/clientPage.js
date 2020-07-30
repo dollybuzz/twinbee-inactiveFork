59a812d50bc3cc500f00fe4b643d8a5ee3ba89b4
Dalia Faria <43527456+dollybuzz@users.noreply.github.com>
2020-07-29 23:29:26 -1000

    precedence if has invoices

--- public/js/clientPage.js
+++ public/js/clientPage.js
@@ -143,33 +143,7 @@ function introMessage() {
 }
 
 function showAlerts() {
-    $.ajax({
-        url: "/api/getAllMyTimeBuckets",
-        method: "post",
-        data: {
-            auth: id_token,
-            token: id_token,
-        },
-        dataType: "json",
-        success: function (bucketres, bucketstatus) {
-            for(var plan in bucketres.buckets) {
-                if (Number.parseInt(bucketres.buckets[plan]) <= 0) {
-                    $("#clientAlerts").html("<div class='alert alert-danger alert-dismissable fade show' role='alert'>You have no hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
-                        "<span aria-hidden='true'>&times;</span></button></div>");
-                    break;
-                } else {
-                    if (Number.parseInt(bucketres.buckets[plan]) <= 300 && Number.parseInt(bucketres.buckets[plan]) > 0) {
-                        $("#clientAlerts").html("<div class='alert alert-warning alert-dismissable fade show' role='alert'>You are running low on available hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
-                            "<span aria-hidden='true'>&times;</span></button></div>");
-                    }
-                }
-            }
-        },
-        error: function (bucketres, bucketstatus) {
-            $("#userMainContent").html("Alerts are not working!");
-        }
-    });
-
+    let hasInvoices = false;
     $.ajax({
         url: "/api/doIHaveInvoices",
         method: "post",
@@ -183,12 +157,44 @@ function showAlerts() {
             {
                 $("#clientAlerts").append(`<div class='alert alert-danger alert-dismissable fade show' role='alert'>You have ${invoiceres.numInvoices} outstanding invoice(s)!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>` +
                     "<span aria-hidden='true'>&times;</span></button></div>");
+                hasInvoices = true;
             }
         },
         error: function (invoiceres, invoicestatus) {
             $("#userMainContent").html("Alerts are not working!");
         }
     });
+
+    if(!hasInvoices)
+    {
+        $.ajax({
+            url: "/api/getAllMyTimeBuckets",
+            method: "post",
+            data: {
+                auth: id_token,
+                token: id_token,
+            },
+            dataType: "json",
+            success: function (bucketres, bucketstatus) {
+                for(var plan in bucketres.buckets) {
+                    if (Number.parseInt(bucketres.buckets[plan]) <= 0) {
+                        $("#clientAlerts").html("<div class='alert alert-danger alert-dismissable fade show' role='alert'>You have no hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+                            "<span aria-hidden='true'>&times;</span></button></div>");
+                        break;
+                    } else {
+                        if (Number.parseInt(bucketres.buckets[plan]) <= 300 && Number.parseInt(bucketres.buckets[plan]) > 0) {
+                            $("#clientAlerts").html("<div class='alert alert-warning alert-dismissable fade show' role='alert'>You are running low on available hours!<button type='button' class='close' data-dismiss='alert' aria-label='Close'>" +
+                                "<span aria-hidden='true'>&times;</span></button></div>");
+                        }
+                    }
+                }
+            },
+            error: function (bucketres, bucketstatus) {
+                $("#userMainContent").html("Alerts are not working!");
+            }
+        });
+    }
+
 }
 
 //Google
